051750aff0308b07bb2b881dbb628cfe186817f8
turab45 <turabbajeer202@gmail.com>
2021-08-02 10:50:10 +0500

    first program

--- springcore/src/main/java/com/springcore/test/App.java
+++ springcore/src/main/java/com/springcore/test/App.java
@@ -1,5 +1,8 @@
 package com.springcore.test;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+        
+        Person person = (Person) context.getBean("person");
+        
+        System.out.println(person);
     }
 }
