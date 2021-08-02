38a81723bf1e830d31a6316e1f7e796290c42e1b
turab45 <turabbajeer202@gmail.com>
2021-08-02 11:34:45 +0500

    Add p schema depedency injection

---
+++ springcore/src/main/java/com/springcore/pschema/Main.java
@@ -0,0 +1,18 @@
+package com.springcore.pschema;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/springcore/pschema/config.xml");
+		
+		Student student = (Student) context.getBean("student");
+		
+		System.out.println(student);
+
+	}
+
+}
