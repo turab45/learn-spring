58f619a172f0767b1e3116becdd8ded90a8e530a
turab45 <turabbajeer202@gmail.com>
2021-08-05 11:56:50 +0500

    Autowiring using @Qualifier annotation

--- springcore/src/main/java/com/springcore/autowiring/annotation/Employee.java
+++ springcore/src/main/java/com/springcore/autowiring/annotation/Employee.java
@@ -1,9 +1,11 @@
 package com.springcore.autowiring.annotation;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 public class Employee {
 	@Autowired
+	@Qualifier("address1") // It is used to inject the particular bean by name
 	private Address address;
 
 	/*To use the constructor injection simply put @AutoWired annotation on the constructor.*/
