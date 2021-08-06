6c813618516a44a0792cae6df8646c6c406a5f33
turab45 <turabbajeer202@gmail.com>
2021-08-06 10:31:25 +0500

    invoke static method, static variable and object in spring expression lang

--- springcore/src/main/java/com/springcore/spel/Demo.java
+++ springcore/src/main/java/com/springcore/spel/Demo.java
@@ -10,6 +10,15 @@ public class Demo {
 	@Value("#{ 10 + 10}")
 	private Integer y;
 	
+	@Value("#{ T(java.lang.Math).sqrt(35)}") // Invoke the static method
+	private double z;
+	
+	@Value("#{ T(java.lang.Math).E}") // Invoke the static variable
+	private double e;
+	
+	@Value("#{ new java.lang.String('Turab Bajeer')}")
+	private String name;
+	
 	public Integer getX() {
 		return x;
 	}
@@ -22,11 +31,31 @@ public class Demo {
 	public void setY(Integer y) {
 		this.y = y;
 	}
-	
+	public double getZ() {
+		return z;
+	}
+	public void setZ(double z) {
+		this.z = z;
+	}
+	public double getE() {
+		return e;
+	}
+	public void setE(double e) {
+		this.e = e;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
 	@Override
 	public String toString() {
-		return "Demo [x=" + x + ", y=" + y + "]";
+		return "Demo [x=" + x + ", y=" + y + ", z=" + z + ", e=" + e + ", name=" + name + "]";
 	}
+	
+	
+	
 	
 	
 	
