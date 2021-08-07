563904d1e88ac24e473a5a55a8a2ee9554a40480
turab45 <turabbajeer202@gmail.com>
2021-08-07 09:17:17 +0500

    Add jdbc template config file

---
+++ spring-jdbc/src/main/java/com/spring/jdbc/Student.java
@@ -0,0 +1,34 @@
+package com.spring.jdbc;
+
+public class Student {
+	private Integer id;
+	private String name;
+	private String city;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", city=" + city + "]";
+	}
+	
+	
+	
+}
