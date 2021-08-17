e816e0054ed7b4a8c5d0f7e6c282c50021476944
turab45 <turabbajeer202@gmail.com>
2021-08-17 10:05:01 +0500

    @PathVariable annoation in spring mvc

--- spring-mvc/src/main/java/com/springmvc/controller/HomeController.java
+++ spring-mvc/src/main/java/com/springmvc/controller/HomeController.java
@@ -2,12 +2,19 @@ package com.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
+	
+	@RequestMapping(path = "/user/{userId}", method = RequestMethod.GET)
+	public String getUserById(@PathVariable("userId") Integer id) {
+		System.out.println(id);
+		return "";
+	}
 	
 	@RequestMapping(path="/home", method = RequestMethod.GET)
 	public String home(Model model) {
